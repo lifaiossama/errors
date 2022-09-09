7206bb847eb27b3b36d3d74d35b0e9f49cb227b9
tozxart <69420764+tozxart@users.noreply.github.com>
2022-09-09 07:41:30 +0100

    fk

---
+++ errors2.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/error/main/error.lua"))()
+shared.key = "ToZx"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Poopland/Alpha/Loader/Alphabit.luarin"))()
