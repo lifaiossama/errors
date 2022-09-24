bad4186ad5e6ec1c8c531d6a71f2963a9e504a1b
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-09-24 09:20:31 +0100

    Update errors2.lua

--- errors2.lua
+++ errors2.lua
@@ -3,5 +3,17 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Poopland/Alpha/Loader
 
 repeat wait() until game:IsLoaded()
 _G.Settings = {}
+if game.PlaceId == 6938803436 or game.PlaceId == 7338881230 or game.PlaceId == 6990129309 or game.PlaceId == 7274690025 or game.PlaceId == 6990131029 or game.PlaceId == 6990133340 then
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/errors/main/errors.lua"))()
+    end)
+end
+else
+repeat wait() until game:IsLoaded()
+_G.Settings = {}
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/errors/main/errors.lua"))()
+if game.PlaceId == 2727067538 then
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/errors/main/WorldZero.Lua"))()
+    end)
+end
