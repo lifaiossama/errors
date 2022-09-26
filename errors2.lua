663ea91247d2f65291445f925b2645639698df14
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-09-26 13:27:03 +0100

    Update errors2.lua

--- errors2.lua
+++ errors2.lua
@@ -1,7 +1,7 @@
 if game.PlaceId == 6938803436 or game.PlaceId == 7338881230 or game.PlaceId == 6990129309 or game.PlaceId == 7274690025 or game.PlaceId == 6990131029 or game.PlaceId == 6990133340 then
     pcall(function()     
 repeat wait() until game:IsLoaded()
-        shared.key = "ToZx"
+       
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Poopland/Alpha/Loader/Alphabit.luarin"))()
 
         loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/errors/main/errors.lua"))()
