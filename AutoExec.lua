88136e39c26118ec2c267748228056d012b47935
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-09-28 02:23:17 +0100

    etst

---
+++ AutoExec.lua
@@ -0,0 +1,3 @@
+
+local queue_on_teleport=syn and syn.queue_on_teleport or queue_on_teleport or queueonteleport;if queue_on_teleport then
+        pcall(function()queue_on_teleport(game:HttpGet"https://raw.githubusercontent.com/lifaiossama/errors/main/errors2.lua")end)
