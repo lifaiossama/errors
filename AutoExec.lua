17b02f1b7020209c39f2ca40db596f5fcffd2eea
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-09-28 02:29:47 +0100

    Delete AutoExec.lua

--- AutoExec.lua
+++
@@ -1,3 +0,0 @@
-
-local queue_on_teleport=syn and syn.queue_on_teleport or queue_on_teleport or queueonteleport;if queue_on_teleport then
-        pcall(function()queue_on_teleport(game:HttpGet"https://raw.githubusercontent.com/lifaiossama/errors/main/errors2.lua")end)
