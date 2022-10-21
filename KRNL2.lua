5b9a823fb8f190d6ddabc3027b5234e13c5af3dc
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-10-21 15:32:12 +0100

    Update KRNL2.lua

--- KRNL2.lua
+++ KRNL2.lua
@@ -1,14 +1,4 @@
 
-    _G.Autotrial  = false; -- Time Challenge
-    _G.Autocustom = false; -- Dimension Farm
-    _G.Autoraid = false; -- Raid
-    _G.FriendsOnly = false;
-    _G.CustomDifficulty = "Easy"; -- "Easy", "Hard", "Nightmare", "Infinite"
-    _G.Raidselectmap = "Nardo Beast Raid"; -- { "Tengu Raid", "Nardo Beast Raid", "Gear 5 Fluffy Raid", "Red Emperor Raid", "Cursed Sage Raid", "Hirito Raid", "Titan Raid"}
-    _G.Hardcore = false;
-    _G.custommapselect = "Titan Dimension"; --("Infinite Mode","Titan Dimension","Demon Dimension","Curse Dimension","Villain Dimension","Sword Dimension","Ghoul Dimension","Fate Dimension","Halloween Dimension","Slime Dimension")
-    _G.Autospeedraid = false; -- Speed raid
-    _G.Autoboss = false; -- Boss Rush
                        while _G.Autotrial == true do -- trial
                         game:GetService("ReplicatedStorage").RemoteFunctions.MainRemoteFunction:InvokeServer(
                             "TeleportToTimeChallenge"
