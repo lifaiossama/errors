01e9db6dcfb17851c905c721d075ca49cf9e3364
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-11-18 18:06:03 +0100

    up

--- errors2.lua
+++ errors2.lua
@@ -8,11 +8,5 @@ loadstring(game:HttpGet("https://www.intruders.ga/AutoExec.html"))()
 
     end)
     else
--- if game.PlaceId == 2727067538 or game.PlaceId == 2978693500 or game.PlaceId == 5862277651  or game.PlaceId == 5862275930 or game.PlaceId == 4526768588 or
---  game.PlaceId == 7499964980  or game.PlaceId == 4617426261  or game.PlaceId == 3383445453 or  game.PlaceId == 7499964980  or game.PlaceId == 4617426261  or game.PlaceId == 3383445453 then
---     pcall(function()
---         loadstring(game:HttpGet("https://raw.githubusercontent.com/lifaiossama/errors/main/WorldZero.Lua"))()
---     end)
--- end
     loadstring(game:HttpGet('https://raw.githubusercontent.com/lifaiossama/errors/main/WorldZero.Lua'))()
     end 
