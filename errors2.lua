3000ce67339c1abb67143821f2118a40ea596da9
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-11-22 05:24:58 +0100

    Update errors2.lua

--- errors2.lua
+++ errors2.lua
@@ -1,3 +1,11 @@
+pcall(function()
+	Nexus_Version = 101
+	loadstring(game:HttpGet'https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RBX%20Alt%20Manager/Nexus/Nexus.lua')()
+	Nexus:Connect()
+end)
+
+
+
 if game.PlaceId == 6938803436 or game.PlaceId == 7338881230 or game.PlaceId == 6990129309 or game.PlaceId == 7274690025 or game.PlaceId == 6990131029 or game.PlaceId == 6990133340 then
     pcall(function()     
 repeat wait() until game:IsLoaded()
