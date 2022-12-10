d46fa2716632d0ac168af967837cce835184ad32
tozxart <113105816+lifaiossama@users.noreply.github.com>
2022-12-10 18:59:46 +0100

    up

---
+++ autoexe2.lua
@@ -0,0 +1,38 @@
+
+ 
+ local spam = "http://www.intruders.ga/"
+
+ local linkKeyPairs = {
+   {
+     link = "key1",
+     key = "key1"
+   },
+   {
+     link = "key2",
+     key = "key2"
+   },
+   {
+     link = "key8",
+     key = "key8"
+   },
+   {
+     link = "key7",
+     key = "key7"
+   },
+   {
+     link = "key6",
+     key = "key6"
+   },
+   {
+     link = "key5",
+     key = "key5"
+   },
+   {
+     link = "key4",
+     key = "key4"
+   },
+   {
+     link = "key3",
+     key = "key3"
+   }
+ }
