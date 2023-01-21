00847af6c780f40c1716f8477079576f513e64e2
tozxart <113105816+lifaiossama@users.noreply.github.com>
2023-01-21 01:26:45 +0100

    Update autoRelunch.lua

--- autoRelunch.lua
+++ autoRelunch.lua
@@ -1,4 +1,4 @@
 local player = game:GetService"Players".LocalPlayer
-player:Kick("Script will stop working for the next days")
+player:Kick("Script will stop working for the next days | Join Discord For more information: https://discord.gg/VJuhSbNxCU")
 wait(15)
 game:Shutdown()
